b47ae53c719320d79fca33782eb132885300b926
sebawo <sebastian@getbase.com>
2015-11-03 23:55:28 +0100

    Adding steps for removing created shipping methods

--- spec/flexible_shipping_spec.rb
+++ spec/flexible_shipping_spec.rb
@@ -7,6 +7,9 @@ describe "Flexible Shipping test scenarios" do
 		on(WpSettingsPage).wait_until { @current_page.settings_page_loaded? } #@current_page means lately used page
 	end
 
+  let(:shipping_method_1) { "List ekononomiczny"}
+  let(:shipping_method_2) { "List ekononomiczny 2"}
+
 	context 'Configuration of general settings' do
 
     it "Go to Woo Settings configuration page" do
@@ -35,7 +38,7 @@ describe "Flexible Shipping test scenarios" do
     it "Add first shipment method" do
       on(FlexibleShippingSettingsPage).add_new_method
       on(AddNewMethodPage).check_enable_method
-      on(AddNewMethodPage).shipping_title = "List ekonomiczny"
+      on(AddNewMethodPage).shipping_title = shipping_method_1
       on(AddNewMethodPage).shipping_zone = "Polska"
       on(AddNewMethodPage).based_on = "Price (zł)"
       on(AddNewMethodPage).range_max = "10"
@@ -50,7 +53,7 @@ describe "Flexible Shipping test scenarios" do
       # on(FlexibleShippingSettingsPage).scroll_to(@current_page.add_new_method_element)
       on(FlexibleShippingSettingsPage).add_new_method_element.when_visible(30).click
       on(AddNewMethodPage).check_enable_method
-      on(AddNewMethodPage).shipping_title = "List ekonomiczny 2"
+      on(AddNewMethodPage).shipping_title = shipping_method_2
       on(AddNewMethodPage).shipping_zone = "Polska"
       on(AddNewMethodPage).based_on = "Price (zł)"
       on(AddNewMethodPage).range_min = "11"
@@ -61,5 +64,13 @@ describe "Flexible Shipping test scenarios" do
     it "Save plugin settings changes" do
       on(FlexibleShippingSettingsPage).save_changes_element.when_visible(30).click
     end
-	end
+
+    it "Delete first shipping method" do
+      on(FlexibleShippingSettingsPage).delete_shipping_method(shipping_method_1)
+    end
+
+    it "Delete second shipping method" do
+      on(FlexibleShippingSettingsPage).delete_shipping_method(shipping_method_2)
+    end
+  end
 end
--- spec/support/flexible_shipping/pages/flexible_shipping_settings_page.rb
+++ spec/support/flexible_shipping/pages/flexible_shipping_settings_page.rb
@@ -4,6 +4,15 @@ class FlexibleShippingSettingsPage
   checkbox(:enable_plugin, id: 'woocommerce_flexible_shipping_enabled')
   link(:add_new_method, class: 'add-new-h2')
   button(:save_changes, class: 'button-primary')
+  table(:shipping_methods_table, class: 'wc_shipping widefat wp-list-table')
+  div(:message_box, id: 'message')
+
+
+  def delete_shipping_method(method)
+    shipping_methods_table_element["#{method}"][11].link_element.click
+    puts self.message_box_element.when_visible.text
+    self.wait_until {self.message_box == "Shipping method #{method} deleted."}
+  end
 
   def select_tax_status(status)
     div_element(id: 's2id_woocommerce_flexible_shipping_tax_status').link_element.click
@@ -14,6 +23,4 @@ class FlexibleShippingSettingsPage
     div_element(id: 's2id_woocommerce_flexible_shipping_condition').link_element.click
     div_element(text: "#{condition}").when_visible.click
   end
-
-
 end
